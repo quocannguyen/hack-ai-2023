dfc3445969b2acb6dca6641c59fadb055cd802e8
salmonwz <jojo.tamm2k@gmail.com>
2023-04-16 06:13:24 -0500

    Updated Animations

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -6,14 +6,16 @@ import { Link } from "react-router-dom";
 const Homepage = () => {
     return(
     <div className= {x.container}>
+        <div className={x.layer}>
             <div className={x.header}>
                 <div className={x.headerLeft}>
                     <h1>ll</h1>
                 </div>
                 <div className={x.headerRight}>
-                   <li><Link to={'login'} style={{textDecoration: 'none', color: 'white'}}>log in </Link></li>
-                    <li><Link to={'signup'} style={{textDecoration: 'none', color: 'white'}}>signup </Link></li>
+                   <li className={x.linkRight}><Link to={'login'} style={{textDecoration: 'none', color: 'white'}}>log in </Link></li>
+                    <li className={x.linkRight}><Link to={'signup'} style={{textDecoration: 'none', color: 'white'}}>signup </Link></li>
                 </div>
+            </div>
             </div>
             <div className={x.midPage}>
                 <div className={x.headerInfo}>
@@ -21,6 +23,17 @@ const Homepage = () => {
             </div>
             <div className={x.diskContainer}>
                <Link to={'input'}> <img className={x.disk} src={cd} alt="A Disk" /> </Link>
+               <div className={x.footer}>
+                    <div className={x.footerNames}>
+                            <h2>Contacts</h2>
+                            <div classNames={x.names}>
+                                <h3>Jojo Tamang</h3>
+                                <h3>Ariel Yong</h3>
+                                <h3>Ann Ngueyn</h3>
+                                <h3>Nishith Guda</h3>
+                            </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
