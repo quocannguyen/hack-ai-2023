59da5980a6b655c2c609e0c7b8b2230e83404dfb
Nishith Guda <68877236+threatdead@users.noreply.github.com>
2023-04-15 19:58:51 -0500

    records audio

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,25 +1,29 @@
-import boto3
-import io
-import pandas as pd
+import pyaudio
+import wave 
+def recordAudio():
+    FORMAT = pyaudio.paInt32
+    CHUNK = 1024
+    RATE = 44100
+    CHANNELS = 1
 
-# Set Buffer
-buffer_pbp = io.BytesIO()
-buffer_players = io.BytesIO()
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT, channels = CHANNELS, rate=RATE, input=True,frames_per_buffer=CHUNK)
 
-# Create connection to S3
-s3 = boto3.resource('s3', aws_access_key_id = 'AKIAWNNDBSXELJDB2NPI', aws_secret_access_key = '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')
+    print("Begin recording")
+    frames = []
+    seconds = int(input('How many seconds do you want to hum for: '))
+    for i in range(0, int(RATE/CHUNK*seconds)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print("End recording")
 
-# Read PBP Data from S3
-pbp_object = s3.Object('utd-hackathon', 'event_pbp.parquet')
-pbp_object.download_fileobj(buffer_pbp)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-df_pbp = pd.read_parquet(buffer_pbp)
-
-print(df_pbp.head())
-
-# Read Players Data from S3
-players_object = s3.Object('utd-hackathon', 'game_players.parquet')
-players_object.download_fileobj(buffer_players)
-
-df_players = pd.read_parquet(buffer_players)
-print(df_players.columns)
+    wf = wave.open("output.wav",'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
